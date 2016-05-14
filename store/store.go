5a714ce3a77d0c40462bd34ed74737d0ecfc0a2a
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2016-05-13 22:02:50 -0400

    Typos and styling

--- store/store.go
+++ store/store.go
@@ -1,33 +1,33 @@
 package store
 
-import(
-   "errors"
+import (
+	"errors"
 )
 
 type Store struct {
-   data map[string]string
+	data map[string]string
 }
 
 func (store *Store) Get(key string) (value string, err error) {
-   value, ok := store.data[key]
+	value, ok := store.data[key]
 
-   if ok {
-      return value, nil
-   }
-   return "", errors.New("No value set")
+	if ok {
+		return value, nil
+	}
+	return "", errors.New("No value set")
 }
 
 func (store *Store) Set(key string, value string) {
-   if store.data == nil {
-      store.Init()
-   }
-   store.data[key] = value
+	if store.data == nil {
+		store.Init()
+	}
+	store.data[key] = value
 }
 
 func (store *Store) Unset(key string) {
-   delete(store.data, key)
+	delete(store.data, key)
 }
 
 func (store *Store) Init() {
-   store.data = make(map[string]string)
+	store.data = make(map[string]string)
 }
--- store/store_test.go
+++ store/store_test.go
@@ -1,53 +1,53 @@
 package store_test
 
 import (
-   "testing"
-   "github.com/ethanaubuchon/lightdm/store"
+	"github.com/ethanaubuchon/lightdm/store"
+	"testing"
 )
 
 func TestGet(t *testing.T) {
-   s := store.Store{}
-   actual, err := s.Get("A")
-   expected := ""
+	s := store.Store{}
+	actual, err := s.Get("A")
+	expected := ""
 
-   if err == nil {
-      t.Error("Expected an error")
-   }
+	if err == nil {
+		t.Error("Expected an error")
+	}
 
-   if actual != expected {
-      t.Error("Expected " + expected + " Actual " + actual)
-   }
+	if actual != expected {
+		t.Error("Expected " + expected + " Actual " + actual)
+	}
 }
 
 func TestSet(t *testing.T) {
-   s := store.Store{}
-   expected := "value"
-   key := "Key"
-   s.Set(key, expected)
-   actual, err := s.Get(key)
+	s := store.Store{}
+	expected := "value"
+	key := "Key"
+	s.Set(key, expected)
+	actual, err := s.Get(key)
 
-   if err != nil {
-      t.Error("Unexpected Error")
-   }
+	if err != nil {
+		t.Error("Unexpected Error")
+	}
 
-   if actual != expected {
-      t.Error("Expected " + expected + " Actual " + actual)
-   }
+	if actual != expected {
+		t.Error("Expected " + expected + " Actual " + actual)
+	}
 }
 
 func TestUnset(t *testing.T) {
-   s := store.Store{}
-   expected := ""
-   key := "Key"
-   s.Set(key, "value")
-   s.Unset(key)
-   actual, err := s.Get(key)
+	s := store.Store{}
+	expected := ""
+	key := "Key"
+	s.Set(key, "value")
+	s.Unset(key)
+	actual, err := s.Get(key)
 
-   if err == nil {
-      t.Error("Expected an error")
-   }
+	if err == nil {
+		t.Error("Expected an error")
+	}
 
-   if actual != expected {
-      t.Error("Expected " + expected + " Actual " + actual)
-   }
+	if actual != expected {
+		t.Error("Expected " + expected + " Actual " + actual)
+	}
 }
